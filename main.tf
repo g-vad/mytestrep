4058a54333fb6a492b755bd5d3128a5a3c82cfd8
g-vad <vasudvn49@gmail.com>
2022-09-12 01:02:27 +0530

    Commit 2 with provider details

---
+++ main.tf
@@ -0,0 +1,24 @@
+#Azure provider source and version
+terraform {
+  required_providers{
+    azurerm = {
+        source = "hashicorp/azure"
+        version = "=3.0.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "Test-RG-TR"
+    storage_account_name = "testdiskbackupimmutable"
+    container_name = "tfstatecnt"
+    key = "myterraform.state"
+  }
+}
+
+#Azure provider configuration/features
+provider "azurerm"{
+    features{}
+}
+
+data "azurerm_storage_account" "storage_detail"{
+    value = azurerm_storage_account.storage_detail
+}
